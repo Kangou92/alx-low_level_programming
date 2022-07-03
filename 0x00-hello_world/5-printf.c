a2d1cceff892c9bf4168a5fd119dab609fb46814
kangou92 <gountanek@gmail.com>
2022-07-03 10:49:22 -0700

    printing a message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * this is the entry point
- *
+ *main entry  point
+ *Return:always 0 (success)
  */
-int main (void)
+int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
