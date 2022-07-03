d1a00e8b41f6d0e066bfe3fbfba0d92fd45fe89f
kangou92 <gountanek@gmail.com>
2022-07-03 09:49:32 -0700

    printing a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- * /
- int main(void)
+/*
+*main - Entry point
+*Return: Always 0 (sucess)
+*
+*/
+ int main (void)
+
+/*Return: Print a message to th screen*/
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
