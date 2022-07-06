4b02d5e7dda636c63300a408071f47a2dce75b9d
kangou92 <gountanek@gmail.com>
2022-07-06 04:35:17 -0700

    printing alphabet letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+/**
+ * main - Entry Point
+ *Return: always 0
+*/
+int main(void)
+{
+char a = 'a';
+	putchar(a);
+	while (a > 'z')
+	putchar(a);
+return (0);
+}
