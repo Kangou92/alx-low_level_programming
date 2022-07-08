6817036ce93b677866a978592dfeb2a8f22acc4a
kangou92 <gountanek@gmail.com>
2022-07-08 05:12:23 -0700

    commenting a part of code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - causes an infinity loop
+ * Return: 0
+*/
+int main(void)
+{
+int i:
+printf("infinite loop incomming :(\n");
+i = 0;
+while (i < 10)
+	{
+		putchar(i);
+	}
+	/*this part of code  is causing the output*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
