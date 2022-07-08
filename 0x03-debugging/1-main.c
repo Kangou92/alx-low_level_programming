73298e352f0b88eb88b9ea9a8dff60700acf13ac
kangou92 <gountanek@gmail.com>
2022-07-08 05:23:57 -0700

    commenting a part of code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,11 @@ int main(void)
 int i:
 printf("infinite loop incomming :(\n");
 i = 0;
+/*this part of code causes the output of infinite loop*/
 while (i < 10)
 {
 	putchar(i);
 }
-	/*this part of code  is causing the output*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
