8e9e541b123babc06eedb484860a1a94e2c46379
kangou92 <gountanek@gmail.com>
2022-07-10 14:15:38 -0700

    printing hexadecimal digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+*/
+
+int main(void)
+{
+char c;
+for (c = '0'; c <= 'F'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
