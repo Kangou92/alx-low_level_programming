da1b06416e3de8fbb5b0d1fb9beec8e84f038ee7
kangou92 <gountanek@gmail.com>
2022-07-10 00:42:56 -0700

    printing alphabet in lower case

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 char c;
+char e = ' '
 for (c = 'a'; c <= 'z'; c++)
 {
 	if (c == 'e' || c == 'q')
 	{
 		continue;
 	}
-	putchar(c);
-	putchar("");
+	putchar(e);
 }
 return (0);
 }
