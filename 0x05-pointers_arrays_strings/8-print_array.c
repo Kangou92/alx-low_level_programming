2c287f7ac1e4537b79004f8b91d60ba4e4a93505
kangou92 <gountanek@gmail.com>
2022-07-12 09:05:30 -0700

    print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ *  print_array - print th element of a
+ *  given array
+ *  @a: array to print
+ *  @n:interger
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0 i < n - 1; i++)
+printf(", %d", a[i]);
+}
