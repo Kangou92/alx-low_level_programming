6a8643e16bf377d1011c91b1869c64c343b6ee96
kangou92 <gountanek@gmail.com>
2022-07-12 00:13:13 -0700

    give the lengh of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * _strlen - give the lenght of a char
+ * @s:parameter which is a char type
+ *Return:always 0
+*/
+
+int _strlen(char *s)
+{
+int i;
+i = strlen(s);
+return (0);
+}
