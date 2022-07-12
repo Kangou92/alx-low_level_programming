46548323b25ad64b355df7a7717da9b3d202c161
kangou92 <gountanek@gmail.com>
2022-07-12 07:21:52 -0700

    print the half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * puts_half - print the half of a string
+ * or the last n character if the length is a odd number
+ * @str:string which half is to printed
+*/
+
+void puts_half(char *str)
+{
+int i, n, j;
+j = strlen(str);
+if (j % 2 == 0)
+{
+	for (i = 0; i < j / 2; i++)
+	{
+		putchar(str[i]);
+	}
+}
+else
+{
+n = (j - 1) / 2;
+for (i = n; i >= 0; i--)
+{
+	putchar(str[i]);
+}
+}
+}
