704daba027f974e24941361cf6ece24bd3fa84a2
kangou92 <gountanek@gmail.com>
2022-07-12 06:48:08 -0700

    put

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * puts2 - print the first character of a given
+ * string with all th other character
+ * @str:string to use
+*/
+
+void puts2(char *str)
+{
+int i, j;
+j = strlen(str);
+putchar(str[0]);
+for (i = 1; i < j - 1; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
