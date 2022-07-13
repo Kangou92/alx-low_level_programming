285b3d7e383350ad4a416de0d8ed475809b60abf
kangou92 <gountanek@gmail.com>
2022-07-13 15:55:56 -0700

    print combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	int i, j, c;
+	int i, j;
 
 	for (i = 48; i <= 57; i++)
 	{
@@ -15,11 +15,7 @@ int main(void)
 		{
 			if (i != j)
 			{
-				while (c != j)
-				{
 				putchar(j);
-				c += 1;
-				}
 				putchar(44);
 				putchar(32);
 			}
