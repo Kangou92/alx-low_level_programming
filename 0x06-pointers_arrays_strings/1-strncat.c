f8289c937b7a007af6d6457f04fba24c2daa39c2
kangou92 <gountanek@gmail.com>
2022-07-14 09:11:23 -0700

    concatenate with srtncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,18 +6,24 @@
  * @dest:pointer to a string dest,first paramerter
  * @src:second parameter poin to string src
  * @n:interger length of dest
+ * Return:pointer
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	j = strlen(dest);
-	for (i = 0; i < j; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (dest[i] != '\0')
+		do {
+		if (src[i] != '\0')
 		{
 			dest[i + j] = src[i];
+			n -= 1;
 		}
+		while (n != 0)
+		}
+
 	}
 return (dest);
 }
