10ef0a0b8c0a41f4336686e28cbb681a5be2098e
kangou92 <gountanek@gmail.com>
2022-07-14 16:11:57 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,6 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	strcmp(s1, s2);
-	if (s1 < s2)
-	{
-		i < 0;
-	}
-	else if (s1 > s2)
-	{
-		i > 0;
-	}
-	else
-	{
-		i = 0;
-	}
+	i = strcmp(s1, s2);
 return (i);
 }
