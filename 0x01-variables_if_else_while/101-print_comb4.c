57ecc0d8fd12981492a2c3df7710f867a47c823a
kangou92 <gountanek@gmail.com>
2022-07-13 17:53:46 -0700

    print three combination digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i != 55 || j != 56 k != 57)
+					if (i != 55 || j != 56 || k != 57)
 					{
 						putchar(44);
 						putchar(32);
