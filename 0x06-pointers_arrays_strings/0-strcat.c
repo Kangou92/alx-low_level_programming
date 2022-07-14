6a4047a095884d382e2117c038f7942503515304
kangou92 <gountanek@gmail.com>
2022-07-14 08:04:25 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,19 +12,6 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-	while (*(dest + i != '\0'))
-	{
-		i++
-	}
-	while (j >= 0)
-	{
-		*(dest + i) = *(src + j);
-		if (*(src + j) == '\0')
-			break;
-		i++;
-		j--;
-	}
+strcat(*dest, *src);
 return (dest);
 }
