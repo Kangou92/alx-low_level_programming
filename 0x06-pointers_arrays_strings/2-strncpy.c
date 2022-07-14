8413e61b1db1ceb3eff460efec9846b5614bf7ff
kangou92 <gountanek@gmail.com>
2022-07-14 14:22:36 -0700

    copying with strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		for ( ; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
 	return (dest);
 }
