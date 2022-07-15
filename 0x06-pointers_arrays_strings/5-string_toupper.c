b99b822a0efbb2d523545cb6f4d64e5f7d2bd1b4
kangou92 <gountanek@gmail.com>
2022-07-15 00:10:08 -0700

    convert in upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include<main.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * string_toupper - convert uppercase in lower
+ * @char: to be converted
+ *Return: char
+*/
+
+char *string_toupper(char *)
+{
+	unsigned char c;
+	toupper(c);
+}
+
