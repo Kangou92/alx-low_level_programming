3ecd7c330fdcb0217eb636a6e752ecc5b61d7d5f
kangou92 <gountanek@gmail.com>
2022-07-15 01:40:43 -0700

    convert to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,12 +12,11 @@ char *string_toupper(char *c)
 {
 	int i = 0;
 
-		while (c[i] != '\0')
+		while (c[i])
 		{
 			if (c[i] >= 97 && c[i] <= 122)
-			{
 				c[i] = c[i] - 32;
-			}
+				i++;
 		}
 	return (x);
 
