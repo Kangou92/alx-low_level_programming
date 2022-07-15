1842943687471ff73d522c62e66b2bd75d4d188c
kangou92 <gountanek@gmail.com>
2022-07-15 14:28:54 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include<string.h>
+/**
+ * cap_string - capitalize all the word of a string
+ * @ch: string
+ * Return:string
+*/
+
+char *cap_string(char *ch)
+{
+	int a = 0;
+
+	while (ch[a])
+	{
+		if (ch[a] <= 9 || ch[a] >= 125)
+		{
+			ch[a] = ch[a] - 32;
+			a++;
+		}
+	}
+return (ch);
+}
+
