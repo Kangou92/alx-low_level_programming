6b821a4280aa057c64f21aeb1812ae7444f89601
kangou92 <gountanek@gmail.com>
2022-07-16 06:09:42 -0700

    positivr or not

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include<stdio.h>
 /**
  *  main - tests function that prints if integer is positive or negative
  *  Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 #endif
