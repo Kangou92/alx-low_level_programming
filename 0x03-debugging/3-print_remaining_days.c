8872837427a6b54f86ff5a94ea54716cd274c0ab
kangou92 <gountanek@gmail.com>
2022-07-16 11:00:50 -0700

    convert

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 	convert_day(month, day);
 	while ((month >= 1 && month <= 12) && (day >= 1 && day <= 31))
 	{
-		if ((year % 100 == 0 && year % 400 != 0) || (year % 4 != 0))
+		if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 		{
 			if (month >= 2 && day >= 60)
 			{
@@ -27,8 +27,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			if (month == 2 && day == 60)
 			{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 			}
 			else
 			{
