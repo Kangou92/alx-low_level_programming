d7866deda1f509cf7d7ceeb0d577063221d382b0
kangou92 <gountanek@gmail.com>
2022-07-16 08:23:47 -0700

    the largest of the three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,42 +9,19 @@
 */
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && b >= c)
-{
-	largest = a;
-}
-else if (a > c && c >= b)
-{
-	largest = a;
-}
-else
-{
-	largest = b;
-}
-else if (c > a && a >= b)
-{
-	largest = c;
-}
-else if (c > b && b >= a)
-{
-	largest = a;
-}
-else
-{
-	largest = c;
-else if (b > a && a >= c)
-{
-	largest = b;
-}
-else if (b > c && c >= a)
-{
-	largest = b;
-}
-else
-{
-	largest = c;
-}
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= c && b >= a)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 return (largest);
 }
