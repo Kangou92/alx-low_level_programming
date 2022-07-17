0b72503243c0cb3adfd209ce2f1d5d424ff45275
kangou92 <gountanek@gmail.com>
2022-07-17 00:38:32 -0700

    print a string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
+ * main - prints _putchar 
  * Return:0
 */
 int main(void)
+
 {
-printf("_putchar\n");
-return (0);
+	printf("_putchar");
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * print_alphabet - begin
+*/
+void print_alphabet(void);
+{
+char a;
+for (a = 'a'; a < 'z'; a++)
+{
+	_putchar(c);
+}
