78b8b1cb377bf50808db36b09b2a5fb0381f1322
kangou92 <gountanek@gmail.com>
2022-07-17 09:52:06 -0700

    test the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - print the sign of a given number
+ * @n:number which sign be test
+ * Return: 1 if positive 0 if null and -1 if negative
+*/
+
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 1;
+	}
+	else if (i == 0)
+	{
+		i = 0;
+	}
+	else
+		i = -1;
+	return (i);
+}
