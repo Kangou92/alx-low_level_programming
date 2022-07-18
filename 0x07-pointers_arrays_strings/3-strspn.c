1ca3a6c24bd3b6c4ed8df9ac4d1ccd8c0f3baee8
kangou92 <gountanek@gmail.com>
2022-07-18 16:34:22 -0700

    length of substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,25 +11,8 @@
 unsigned int _strspn(char *s, char *accept);
 
 {
-	unsigned int i = 0, j k, l, len = 0, p;
-
-	j = strlen(s);
-	k = strlen(accept);
-	while (s[i])
-	{
-	for (i = 0; i < j; i++)
-	{
-		for (l = 0; l < k; l++)
-		{
-		if (s[i] == accept[j]
-		{
-			len += 1;
-			p = sizeof(len);
-			break;
-		}
-		else
-		break;
-		}
-	}
+	unsigned int i, len;
+	i = strspn(s, accept);
+	len = sizeof(i);
 	return (len);
 }
