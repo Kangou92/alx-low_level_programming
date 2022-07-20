19b8fce2a5ee3c90a0bd2b80b33746545c292c4f
kangou92 <gountanek@gmail.com>
2022-07-20 04:20:34 -0700

    search a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,12 +10,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; *(haystack + i) != '\0'; i++)
+	for (i = 0; *(needle + i) != '\0'; i++)
 	{
-		for (j = 0; *(needle + j) != '\0'; j++)
+		for (j = 0; *(haystack + j) != '\0'; j++)
 		{
-			if (*(haystack + i) == *(needle + j))
-			{
+			if (*(needle + i) == *(haystack + j))
+		}
 			break;
 			return (needle + j);
 			}
