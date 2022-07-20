dacddff277c6c170c6988365c76080c20fbc12f2
kangou92 <gountanek@gmail.com>
2022-07-20 00:33:47 -0700

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; *(haystack + i) != '\0'; i++)
 	{
-		for (j = 0; *(needle + j) != '\0')
+		for (j = 0; *(needle + j) != '\0'; j++)
 		{
 		if (*(haystack + i) == *(needle + j))
 		{
