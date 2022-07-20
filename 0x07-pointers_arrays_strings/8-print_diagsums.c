45504f092b9346ab35c588940cc472bf326eed8b
kangou92 <gountanek@gmail.com>
2022-07-20 01:05:44 -0700

    diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagsums - print the sum of element on the diagonal
+ * @a:array
+ * @size: integer
+ * Return:void
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, diag_sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				diag_sum = diag_sum + *(a + i) + *(a + j);
+			}
+		}
+	}
+	printf("%d\n", diag_sum);
+}
