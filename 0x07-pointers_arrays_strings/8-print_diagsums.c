9c349402a3d8675ba2fb7ad4e7f20bd9dd8bb701
kangou92 <gountanek@gmail.com>
2022-07-20 03:15:45 -0700

    print diagonal sums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,27 +9,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, diag_sum1 = 0, diag_sum2 = 0;
+	int i, diag_sum1 = 0, diag_sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				diag_sum1 = diag_sum1 + *(a + i );
-			}
-		}
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		for (j = size - 1; j >= 0; j--)
-		{
-			if (i == j)
-			{
-				diag_sum2 = diag_sum2 + *(a + j);
-			}
-		}
+	diag_sum1 = *(a + (size * i + i));
+	diag_sum2 = sum2 += *(a + (size * i + size - 1 - i));	
 	}
 	printf("%d, %d\n", diag_sum1, diag_sum2);
 }
