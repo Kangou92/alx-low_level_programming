c790e8ae65b21d4b98bd4ad06abd76ed659fe1f5
kangou92 <gountanek@gmail.com>
2022-07-21 07:44:41 -0700

    print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+*/
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
