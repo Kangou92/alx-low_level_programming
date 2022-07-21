b70da2a6e041732b92ea8173acce6978463c218d
kangou92 <gountanek@gmail.com>
2022-07-20 17:05:19 -0700

    x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,13 +13,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (x == 0)
-	
+	else if (y == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (x * _pow_recursion(x, (y - 1)));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
