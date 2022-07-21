8821be11edf68d5428276613ad0d845804cf3226
kangou92 <gountanek@gmail.com>
2022-07-21 07:59:58 -0700

    print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * main - entry point
  * Return: always 0
 */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
+int main(void
+		putchar("and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
