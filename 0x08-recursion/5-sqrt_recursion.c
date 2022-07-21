7b34722a61314ca33ae04b5fac7536c31441559a
kangou92 <gountanek@gmail.com>
2022-07-20 17:30:08 -0700

    square root of integer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the natural square root of a given number
+ * @n: integer
+ * Return: integer
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (1 + _sqrt_recursion(n - 1));
+	}
+}
