9321caf4afc6fbd2255b02c5d601800e96323ec3
kangou92 <gountanek@gmail.com>
2022-07-23 09:30:12 -0700

    using argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdio.h>
+
+/**
+ * main - print its own name
+ * @argc: integer
+ * @argv: array of string
+ * Return: void
+*/
+
+void main(int argc, char* argv[])
+{
+	printf("%s",argv[0]);
+}
