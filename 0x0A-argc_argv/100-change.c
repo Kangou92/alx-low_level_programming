957aaccacab0ab949ea9cac134aca6a05260ee99
kangou92 <gountanek@gmail.com>
2022-07-24 10:01:44 -0700

    changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,110 +1,38 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-#include <ctype.h>
-
-
-
 /**
+ * main - prints the minimum number of coins to make change
  *
- *  * main - prints the minimum number of coins
- *
- *   * to make change for an amount of money
- *
- *    * @argc: n args
- *
- *     * @argv: arr args
- *
- *      * Return: 0
- *
- *       */
+ * @argc: integer
+ * @argv: array
+ * Return: 0
+*/
 
 int main(int argc, char *argv[])
-
 {
+	int i, j, k, l = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-		int value, c;
-
-
-
-			c = 0;
-
-				if (argc != 2)
-
-						{
-
-									printf("Error\n");
-
-											return (1);
-
-												}
-
-					value = atoi(argv[1]);
-
-						if (value < 0)
-
-								{
-
-											printf("%d\n", 0);
-
-													return (0);
-
-														}
-
-							if (value % 25 >= 0)
-
-									{
-
-												c += value / 25;
-
-														value = value % 25;
-
-															}
-
-								if (value % 10 >= 0)
-
-										{
-
-													c += value / 10;
-
-															value = value % 10;
-
-																}
-
-									if (value % 5 >= 0)
-
-											{
-
-														c += value / 5;
-
-																value = value % 5;
-
-																	}
-
-										if (value % 2 >= 0)
-
-												{
-
-															c += value / 2;
-
-																	value = value % 2;
-
-																		}
-
-											if (value % 1 >= 0)
-
-													{
-
-																c += value / 1;
-
-																	}
-
-												printf("%d\n", c);
-
-													return (0);
-
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		j = atoi(argv[1]);
+		if (j % coins[i] == 0)
+		{
+			k = j / i;
+			l += k;
+			printf("%d\n", l);
+		}
+	}
 }
-
