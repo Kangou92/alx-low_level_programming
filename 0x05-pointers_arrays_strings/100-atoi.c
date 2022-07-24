21da9a589c25efc1d112cd96c8da1539996db64b
kangou92 <gountanek@gmail.com>
2022-07-23 23:56:48 -0700

    ascii to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _atoi -converts ascii code to integer
+ * @s: array of character
+ * Return: 0
+*/
+
+int _atoi(char *s)
+{
+	int sgn = 1;
+	unsigned int num = 0;
+	do {
+		if (*s == '-')
+			sgn *= -1;
+			else if (*s >= '0' && *s <= '9')
+				num = (num * 10) + (*s - '0');
+			else if (num > 0)
+				break;
+	} while (*s++);
+	return (num * sgn);
+}
