6002fde0eac2db717dbcd6b4f2edfcbc270d22a3
kangou92 <gountanek@gmail.com>
2022-07-25 14:40:32 -0700

    first use of malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,12 +20,13 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	else
+	if (ch == NULL)
 	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		*(ch + i) = c;
-	}
 	}
 		return (ch);
 }
