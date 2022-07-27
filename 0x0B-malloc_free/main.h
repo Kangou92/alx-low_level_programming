3fa25472a85a03c4ea732c07883c6cf204fb48eb
Kangou92 <gountanek@gmail.com>
2022-07-27 09:08:43 +0000

    using malloc

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
 char *_strdup(char *str);
-=======
 char *create_array(unsigned int size, char c);
->>>>>>> 6002fde0eac2db717dbcd6b4f2edfcbc270d22a3
 #endif
