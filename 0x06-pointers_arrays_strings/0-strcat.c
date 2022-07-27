e00d0cd563995b6f16dd12b56aa4e1fd11ef5b7e
Kangou92 <gountanek@gmail.com>
2022-07-27 07:44:46 +0000

    using malloc

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"main.h"
+/**
+ *_strcat - concatenates two string
+ *@dest:first string to be concatenate with the second string
+ *@src: second string to be concatenate with the first string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest src);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learning more on pointers,arrays and string
