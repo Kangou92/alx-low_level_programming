8002fab5fc8107d6409525830bc9cb7dee2be867
Kangou92 <gountanek@gmail.com>
2022-07-29 07:39:53 +0000

    string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,35 +12,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = n, index;
-	char *ch;
-
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
+		s2 = "";
+	while (s1[size1] != '\0')
 	{
-		s2 == "";
+		size1++;
 	}
-	for (index = 0; s1[index]; index++)
-		len++;
-		ch = (char *)malloc(sizeof(char) * len + 1);
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+	if (p == NULL)
+		return (0);
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
 
-	if (ch == NULL)
-
-		return (NULL);
-		len = 0;
-
-	for (index = 0; s1[index]; index++)
-
-		ch[len++] = s1[index];
-
-	for (j = 0; s2[index] && index < n; index++)
-
-		ch[len++] = s2[index];
-
-	ch[len] = '\0';
-
-	return (ch);
+	return (0);
 }
