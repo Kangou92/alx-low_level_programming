2fce29504639325991182b3bbc8cc15b5caa8687
Kangou92 <gountanek@gmail.com>
2022-08-01 21:51:52 +0000

    declaring a structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * dog - presents the declaration of the structure
+ * @name:string
+ * @age: integer
+ * @owner: string
+ *
+ * Description: Presents the good method to declare and comment
+ * a bloc of structure
+*/
 typedef struct dog
 {
 	char *name;
