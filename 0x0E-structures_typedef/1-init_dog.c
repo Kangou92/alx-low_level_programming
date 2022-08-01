1a8537751c03f9134c52e9cc6ac63d554c14a73a
Kangou92 <gountanek@gmail.com>
2022-08-01 23:17:28 +0000

    instantiation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - instantiation of a dog
@@ -14,6 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
+	if (d)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
