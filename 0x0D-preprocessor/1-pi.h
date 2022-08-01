0e3ea264357d4956bc6508afc638d4604ef1e79c
Kangou92 <gountanek@gmail.com>
2022-08-01 11:32:02 +0000

    using macro

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,2 +1,10 @@
-#include <stdio.h>
+#ifndef PI_AS_MACRO
+#define PI_AS_MACRO
+
+/**
+ * 1-pi.h defines a macro PI
+ * as an abbreviation of the token
+ * 3.14159265359
+*/
 #define PI 3.14159265359
+#endif
