6087eb2a9f1ed606d04b5a471eaa201817cbbd7d
Kangou92 <gountanek@gmail.com>
2022-08-01 11:26:19 +0000

    macro file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,9 +2,9 @@
 #define FILE_MACRO
 /**
  * O-object_like_macrO.h
- * defines a macro named SIZE 
+ * defines a macro named SIZE
  * as an abbreviation of
- * 	the token 1024
+ * the token 1024
 */
 
 #define SIZE 1024
