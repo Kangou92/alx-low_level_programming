fab3e8372acee562dc46dff2183837f83a0ec335
Kangou92 <gountanek@gmail.com>
2022-08-01 11:49:53 +0000

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+#define SIZE 0
+
+/**
+ * main - prints the name of the file
+ * it was compiled from
+ * argv: array
+ * argc: integer
+ * Return: 0
+*/
+#endif
+
+int main(int *argc, char *argv[])
+{
+	printf("%s\n", argv[SIZE]);
+	return (0);
+}
