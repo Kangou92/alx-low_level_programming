82a8029f59fd8babff0a0b70b4488bd9f9721226
Kangou92 <gountanek@gmail.com>
2022-08-02 09:27:00 +0000

    initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib.h>
+
 /**
  * init_dog - instantiation of a dog
  * @name: string
@@ -13,7 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
