6233203affe5e208252b607f120f8bd444396498
Kangou92 <gountanek@gmail.com>
2022-08-02 23:20:20 +0000

    free the memory

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 
 /**
- * print_dog - prints rhe information for a given dog
+ * print_dog - prints the information for a given dog
  * @d: an instance of the dog to print
- * Return: void
+ * Return: noi
 */
 
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!(d->name))
-		{
-			printf("Name:(nil)\n");
-		}
-		else
+		if (d->name != NULL)
 		{
 			printf("Name: %s\n", d->name);
 		}
-			printf("Age: %f\n", d->age);
-		if (!(d->owner))
+		else
 		{
-			printf("Owner:(nil)\n");
+			printf("Name: (nil)\n");
+		}
+			printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
 		}
 		else
 		{
-			printf("Owner: %s\n", d->owner);
+			printf("Owner: (nil)\n");
 		}
 	}
 }
