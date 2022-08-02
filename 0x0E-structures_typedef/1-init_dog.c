b2d9419261b8718d50ccb73a4dc799b8adabb7a1
Kangou92 <gountanek@gmail.com>
2022-08-02 07:46:09 +0000

    initiation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,8 +13,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-	d = malloc(sizeof(struct dog));
-	if (d != NULL)
+	struct dog dogi;
+	struct dog *d = &dogi;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
