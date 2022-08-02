e92a800a530f4239f3351fef581ba4bd51de80af
Kangou92 <gountanek@gmail.com>
2022-08-02 22:04:21 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: string
+ * @age: integer
+ * @owner: string
+ * Return: pointer
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i = 0, j = 0, k;
+	dog_t *milou;
+
+	while (name)
+		i++;
+	while (owner)
+		j++;
+	milou = malloc(sizeof(dog_t));
+	if (milou == NULL)
+	{
+		return (NULL);
+	}
+	milou->name = malloc(i * sizeof(char));
+	if (milou->name == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		milou->name[k] = name[k];
+	milou->age = age;
+	milou->owner = malloc(j * sizeof(char));
+	if (milou->owner == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		milou->owner[k] = owner[k];
+	return (milou);
+	free(milou);
+}
+
