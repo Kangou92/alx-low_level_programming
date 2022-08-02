993527a96f91cc65cbcff8483076fc6d03b32a46
Kangou92 <gountanek@gmail.com>
2022-08-02 07:56:41 +0000

    initiation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
-	struct dog dogi;
-	struct dog *d = &dogi;
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	*d->name = name;
+	*d->age = age;
+	*d->owner = owner;
 }
