180257e369ba4466345cbcdc3ac352109dcb551d
Kangou92 <gountanek@gmail.com>
2022-08-02 14:10:31 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: Presents the good method to declare and comment
  * a bloc of structure
 */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
