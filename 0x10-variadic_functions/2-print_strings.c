7e983fc6cb80eba838999789b5849cf39cf59af5
Kangou92 <gountanek@gmail.com>
2022-08-05 14:46:50 +0000

    prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,6 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (str == NULL)
 			{
 				printf("(nil)");
+				printf("%s", separator);
 			}
 			else if (i < n - 1)
 			{
