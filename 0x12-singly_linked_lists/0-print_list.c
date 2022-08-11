ee6e37e1697598c141fac17f528772dfe20ae7b0
Kangou92 <gountanek@gmail.com>
2022-08-11 20:59:31 +0000

    begin with linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,20 @@
 #include "list.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+*/
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 {
@@ -13,7 +27,7 @@ size_t print_list(const list_t *h);
 	{
 		i += 1;
 		h = h->next;
-		printf("%s", str);
+		printf("%s", h->str);
 	}
 	return (i);
 }
