17d7bb63e0c73d5ea81d7695a398c56e68b6b2a9
Kangou92 <gountanek@gmail.com>
2022-08-11 23:03:03 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,12 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	if (h->str == NULL)
-		printf("[0] (nil)\n");
+		printf("[0] (nil)");
 	else
 	while (h)
 	{
 		i += 1;
-		printf("[%lu]%s", i, h->str);
+		printf("[%u]%s", h->len, h->str);
 		h = h->next;
 	}
 	return (i);
