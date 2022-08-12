6a2d4926d0d2f8a4ee2a0703947ff31386d21df8
Kangou92 <gountanek@gmail.com>
2022-08-12 21:56:30 +0000

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,11 +22,12 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 	{
 		return (NULL);
+		free(new);
 	}
 	new->str = strdup(str);
 	new->len = len;
 	new->next = *head;
 	*head = new;
-
 	return (new);
+	free(new);
 }
