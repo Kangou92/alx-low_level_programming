b0c3895a95f4cff97c5f8d36f19815c2116c3bd8
Kangou92 <gountanek@gmail.com>
2022-08-16 23:01:53 +0000

    printing list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the element of the list
+ *
+ * @h: list to be printed
+ * Return: number of element in the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		len++;
+		h = h->next;
+	}
+return (len);
+}
