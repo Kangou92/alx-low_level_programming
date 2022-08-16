cdd44db302b8f2e03203001879379da79eaf1c1e
Kangou92 <gountanek@gmail.com>
2022-08-16 23:00:41 +0000

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listsint_len - returns the length of the list
+ * listint_len - returns the length of the list
  *
  * @h: pointer to the list
  * Return: length of the list
@@ -11,6 +11,8 @@ size_t listint_len(const listint_t *h)
 {
 	size_t len = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h)
 	{
 		len++;
