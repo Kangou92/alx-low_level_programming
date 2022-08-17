59f05a271de52dc30beadc0994d63e8b4a74351e
Kangou92 <gountanek@gmail.com>
2022-08-17 23:34:29 +0000

    deletes head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - Delete the first element of a singly linked list.
+ * @head: Pointer to a list.
+ * Return: Integer if success.
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	*head = tmp->next;
+	data = tp->n;
+	free(tmp);
+return (data);
+}
