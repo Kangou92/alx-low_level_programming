98cb70cb359f13abf0976f69bcf8f00166c3d313
Kangou92 <gountanek@gmail.com>
2022-08-18 02:18:49 +0000

    deleting

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (current->next == NULL)
 			return (-1);
-			current = current->next;
+		current = current->next;
 	}
 	next = current->next;
 	current->next = next->next;
