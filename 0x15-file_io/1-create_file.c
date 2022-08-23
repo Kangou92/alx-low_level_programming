4986f0b5a9251d25195804149f7b751cde886dcb
Kangou92 <gountanek@gmail.com>
2022-08-23 02:04:12 +0000

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * create_file - create a file named filename
+ *
+ * @filename: name of the file
+ * @text_content: content of the file
+ * Return: 1 if success and -1 if not
+*/
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, k, l = 0;
+
+	if (!filename)
+		return (-1);
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (i < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (*(text_content + k))
+		{
+			k++;
+			l = write(i, text_content, k);
+			if (l != k)
+				return (-1);
+		}
+	}
+	close (i);
+	return (1);
+}
+
