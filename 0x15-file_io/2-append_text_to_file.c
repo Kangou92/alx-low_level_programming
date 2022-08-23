67e097d83575aed2f45ca37aa4a71f40e995a905
Kangou92 <gountanek@gmail.com>
2022-08-23 21:12:50 +0000

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (!filename)
 			return (-1);
 		f = open(filename, O_WRONLY | O_APPEND);
-		if (fd < 0)
+		if (f < 0)
 			return (-1);
 		if (text_content)
 		{
